225c9c3af46f0d7547f06a50ae0fdd4cb7c789a0
tororo_1066 <panshoku4@gmail.com>
2026-09-13 04:55:31 +0900

    update social card image and modify description for clarity in documentation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/img.png',
     navbar: {
       title: 'DisplayMonitor',
       logo: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="DisplayMonitorを使うためのドキュメント">
       <HomepageHeader />
       {/*<main>*/}
       {/*  <HomepageFeatures />*/}
